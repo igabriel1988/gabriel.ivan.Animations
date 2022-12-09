4a0ecdc48c5b47fe78ac9273969cc8911a8bfff2
Ivan Gabriel <igabrielcorreopersonal2@gmail.com>
2022-12-09 13:25:03 -0800

    Initial Commit

---
+++ gabriel.ivan.Animations/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  gabriel.ivan.Animations
+//
+//  Created by Ivan Gabriel on 09/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
